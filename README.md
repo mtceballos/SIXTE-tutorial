8249bbf754c30811eacd34b2254fe6c86e64343b
Maite Ceballos <ceballos@ifca.unican.es>
2022-03-22 12:08:12 +0100

    Version adapted to a SIXTE workshop: - Unify wrapper call of SIXTE commands in python (run_comm) - Remove extra functions from funcs.py - Change color maps in imshow images to make them more similar to the manual

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # SIXTE-tutorial
-Practical examples and exercises for the SIXTE tutorial
+Jupyter notebooks to illustrate and document practical examples and exercises for the tutorial part of the [SIXTE](http://www.sternwarte.uni-erlangen.de/research/sixte/index.php) simulator manual.
+
+To use these notebooks you must have SIXTE and [NASA's HEASOFT](https://heasarc.gsfc.nasa.gov/lheasoft/) installed and initiated.
+
+In addition, it is recommended that you create a `conda` environment:
+
+    >conda create --name tutorial python  
+    >conda activate tutorial    
+    >conda install matplotlib jupyterlab pandas astropy
+
+In some environments you need to install also this package to avoid compatibility issues with xspec/heasoft:
+
+    >conda install -c conda-forge gxx_linux-64==11.1.0  
+    >conda install krb5 curl
+
+A different set of additional packages may be required by your specific local configuration
+
+
+
--- funcs.py
+++ funcs.py
@@ -37,10 +37,11 @@ def run_comm(comm, msg=""):
 def draw_second_axis(fig, axs, sq_downleft, sq_upright, ln, mov_ra=0, mov_dec=0, lon_lines=15, num_lines=10, mov_x_text=(0, 0), mov_y_text=(0, 0), xlabel='RA', ylabel='Dec', 
                      num_axis=0, dist_txt=10, zero_between=False, display_format='.2f'):
     """
-    Paints a second axis on the plot for the right ascension (x-axis) and the declination (y-axis). It is prepared to paint with the RA incresing either to the left
+    Paints a second axis on the plot for the right ascension (x-axis) and the declination (y-axis). It is prepared to paint with the RA increasing either to the left
     or right (and the same for the Dec). It is also prepared to work with RA increasing from 0 to 360, so it works even if 360 is reached before the end of the 
-    plot. The Dec is given in a -180º to 180º scale. The functions paints values equally separated, with the number of those given by the user. IMPORTANT: the function only works with 
-    axis and fig objects which are given to the function as parameters after being created. Sintax of the type plt.plot, plt.xlabel ... is not allowed
+    plot. The Dec is given in a -180º to 180º scale. The function paints values equally separated, with the number of those given by the user. 
+    IMPORTANT: the function only works with axis and fig objects which are given to the function as parameters after being created. 
+    Sintax of the type plt.plot, plt.xlabel ... is not allowed
 
     Parameters
     ----------
@@ -54,7 +55,7 @@ def draw_second_axis(fig, axs, sq_downleft, sq_upright, ln, mov_ra=0, mov_dec=0,
     sq_upright : tuple or list
         vector containing the coordinates of the up right square of the plot in the form: (RA, Dec).
     ln : int
-        length of the x-axis (should be equal to the y-axis one) i.e. number of p\'ixels in one line'.
+        length of the x-axis (should be equal to the y-axis one) i.e. number of pixels in one line'.
     mov_ra : float, optional
         moves the RA coordinates to the right (lower values, even negatives) or left (higher values). Values between [-10, 20] are probably needed, though any value out of the interval
         can be used. The default is 0.
@@ -131,94 +132,5 @@ def draw_second_axis(fig, axs, sq_downleft, sq_upright, ln, mov_ra=0, mov_dec=0,
     # Plot both labels 
     axs[num_axis].text(ln / 2 + mov_x_text[0], ln + mov_x_text[1], xlabel) 
     axs[num_axis].text(ln + mov_y_text[0], ln / 2 + mov_y_text[1], ylabel)
-    
-    
-def get_zoom_coord(sq_dwl, sq_upr, ln, x, y, zero_between=False):
-    """    
-    Returns the coordinates (RA, Dec) of the down left square and the up right one
-    of a zoomed area, delimited by pixel numbers given as parameters.
-    Parameters
-    ----------
-    sq_dwl : tuple or list
-        vector containing the coordinates of the down left square of the plot in the form: (RA, Dec).
-    sq_upr : tuple or list
-        vector containing the coordinates of the up right square of the plot in the form: (RA, Dec).
-    ln: int
-        number of pixels in one line in the original plot. IMPORTANT: Not to be confused with the zoomed plot length
-    x : tuple or list
-        contains the x-interval that wants to be zoomed. For example, [700, 900] for the pixels between 700 and 900 (in the x-axis).
-    y : tuple or list
-        contains the y-interval that wants to be zoomed. For example, [700, 900] for the pixels between 700 and 900 (in the y-axis).
-    zero_between : bool, optional
-        True if the RA passes through 0º before the end of the axis. False if not. The default is False.
-
-    Returns
-    -------
-    sq1 : tuple
-        coordinates of the down left square. Format: (x(RA), y(Dec)) .
-    sq2 : tuple
-        coordinates of the up right square. Format: (x(RA), y(Dec)) .
-
-    """
-    m = 1
-    m2 = 1
-    # If zero is between, the calculation of the step needs an adition of 360 to the lower value which can be either the left one or the right one (works with RA increasing or decreasing 
-    # to the right). This is corrected via the m and m2 variables (whether the step is added or substracted from the initial value) 
-    if zero_between:
-        if (sq_upr[0] > sq_dwl[0]):
-            m = -1
-            aux = sq_dwl[0] + 360
-            a = np.array([aux, sq_dwl[1]])
-            step = (a - sq_upr)/ln
-            
-        elif (sq_upr[0] < sq_dwl[0]):
-            aux = sq_upr[0] + 360
-            a = np.array([aux, sq_upr[1]])
-            step = (sq_dwl - a)/ln
-
-    else:
-        step = (sq_dwl - sq_upr)/ln
-        if sq_dwl[0] > sq_upr[0]:
-            m = -1  
-    if sq_dwl[1] > sq_upr[1]:
-        m2 = -1
-    sq1 = np.array((m * step[0] * x[0] + sq_dwl[0], m2 * step[1] * y[1] + sq_upr[1]))
-    sq2 = np.array((m * step[0] * x[1] + sq_dwl[0], m2 * step[1] * y[0] + sq_upr[1]))
-    # If there is any value higher than 360 is corrected (or lower than zero)
-    if sq1[0] > 360:
-        sq1[0] = sq1[0] - 360
-    if sq1[0] < 0:
-        sq1[0] = sq1[0] + 360
-    if sq2[0] < 0:
-        sq2[0] = sq2[0] + 360
-    if sq2[0] > 360:
-        sq2[0] = sq2[0] - 360
-    return sq1, sq2
-
-
         
-def img_subplot(fig, axs, a, b, route='./'):
-    """
-    Saves only one subplot from a plt.subplots object
-
-    Parameters
-    ----------
-    fig : object
-        figure object.
-    axs : object
-        axis object.
-    a : float
-        increase the area (vertical) to let the label in (values like 1.1, 1.2...).
-    b : float
-        increase the area (horizontal) to let the label in (values like 1.1, 1.2...).
-    route : sre, optional
-        route to the directory where the image is going to be saved. The default is './'.
-
-    Returns
-    -------
-    None.
-
-    """
-    extent = axs.get_window_extent().transformed(fig.dpi_scale_trans.inverted())
-    fig.savefig(route, bbox_inches=extent.expanded(a, b), dpi=720)
     
